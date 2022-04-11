52db2514ed5906217295e3f7889516517e59c47c
Marco Alberto <iscgama@gmail.com>
2022-04-11 11:19:52 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeroeModule } from './heroes/heroe.module';
+import { ContadorModule } from './contador/contador.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
